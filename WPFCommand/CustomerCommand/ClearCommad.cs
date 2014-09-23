ccd5b7d4b388ee78ca7b4610c6e79dd89f24dc71
erdao <565130342@qq.com>
2014-09-24 07:18:03 +0800

    test folder
    this is a test folder

---
+++ WPFCommand/CustomerCommand/ClearCommad.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Windows.Input;
+
+namespace CustomerCommand
+{
+    class ClearCommad:ICommand
+    {
+ 
+       public  event EventHandler CanExecuteChanged;
+
+
+        public void Execute(object parameter)
+        {
+            var view = parameter as IView;
+            if (view != null)
+            {
+                view.Clear();
+            }
+        }
+
+
+        public bool CanExecute(object parameter)
+        {
+            throw  new Exception();
+        }
+    }
+}
---
+++ WPFCommand/CustomerCommand/MyCommandSource.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CustomerCommand
+{
+    public class MyCommandSource : UserControl, ICommandSource
+    {
+        public ICommand Command { get; set; }
+
+
+        public object CommandParameter { get; set; }
+
+
+        public IInputElement CommandTarget { get; set; }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+                      
+            if (CommandTarget != null)
+            {
+                Command.Execute(this.CommandTarget);
+            }
+        }
+
+        //protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        //{
+        //    base.OnMouseLeftButtonDown(e);
+
+        //    if (CommandTarget != null)
+        //    {
+        //         Command.Execute(this.CommandTarget);
+        //    }
+        //}
+    }
+}
